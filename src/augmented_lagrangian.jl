db904d712e74ae781460891f428645fe13946318
Brian Jackson <bjack205@gmail.com>
2020-10-02 11:22:09 -0400

    Got augmented lagrangian derivatives

--- src/augmented_lagrangian.jl
+++ src/augmented_lagrangian.jl
@@ -1,3 +1,6 @@
+include("problem.jl")
+include("newton_solver.jl")
+
 function Πsoc(x)
     v = x[1:end-1]
     s = x[end]
@@ -18,6 +21,79 @@ function in_soc(x)
     s = x[end]
     a = norm(v)
     return a <= s
+end
+
+function jac_soc(x)
+    n = length(x)
+    J = zeros(eltype(x), n,n)
+    jac_soc!(J, x)
+    return J
+end
+
+function jac_soc!(J, x)
+    n = length(x)
+    s = x[end]
+    v = x[1:end-1] 
+    a = norm(v)
+    if a <= -s                               # below cone
+        J .*= 0
+    elseif a <= s                            # in cone
+        J .*= 0
+        for i = 1:n
+            J[i,i] = 1.0
+        end
+    elseif a >= abs(s)                       # outside cone
+        # scalar
+        b = 0.5 * (1 + s/a)   
+        dbdv = -0.5*s/a^3 * v
+        dbds = 0.5 / a
+
+        # dvdv = dbdv * v' + b * oneunit(SMatrix{n-1,n-1,T})
+        for i = 1:n-1, j = 1:n-1
+            J[i,j] = dbdv[i] * v[j]
+            if i == j
+                J[i,j] += b
+            end
+        end
+
+        # dvds
+        J[1:n-1,n] .= dbds * v
+
+        # ds
+        dsdv = dbdv * a + b * v / a 
+        dsds = dbds * a
+        # ds = push(dsdv, dsds)
+        ds = [dsdv; dsds]
+        J[n,:] .= ds
+    else
+        throw(ErrorException("Invalid second-order cone projection"))
+    end
+    return J
+end
+
+function hess_soc(x,b)
+    n = length(x)
+    s = x[end]
+    v = x[1:end-1] 
+    bs = b[end]
+    bv = b[1:end-1] 
+    a = nv = norm(v)
+
+    if a <= -s
+        return zeros(n,n)
+    elseif a <= s
+        return zeros(n,n)
+    elseif a > abs(s)
+        dvdv = -s/norm(v)^2/norm(v)*(I - (v*v')/(v'v))*bv*v' + 
+            s/norm(v)*((v*(v'bv))/(v'v)^2 * 2v' - (I*(v'bv) + v*bv')/(v'v)) + 
+            bs/norm(v)*(I - (v*v')/(v'v))
+        dvds = 1/norm(v)*(I - (v*v')/(v'v))*bv;
+        dsdv = bv'/norm(v) - v'bv/norm(v)^3*v'
+        dsds = 0
+        return 0.5*[dvdv dvds; dsdv dsds]
+    else
+        throw(ErrorException("Invalid second-order cone projection"))
+    end
 end
 
 function dual_update_eq(y, ceq, μ)
---
+++ test/derivatives.jl
@@ -0,0 +1,127 @@
+using ForwardDiff
+include("../src/augmented_lagrangian.jl")
+include("../src/di_problem.jl")
+
+function auglag(prob, x,y,z,μ)
+    ceq = con_eq(prob,x)
+    L0 = cost(prob, x) + y'ceq + 0.5*μ*ceq'ceq
+
+    p = num_cones(prob)
+    if p > 0
+        # csoc = [Πsoc(zi - μ*q) for (zi,q) in zip(z, q(x))]
+        cones = con_soc(prob, x)
+        csoc = [Πsoc(z[i] - μ*cones[i]) for i = 1:p]
+        pen = [csoc[i]'csoc[i] - z[i]'z[i] for i = 1:p]
+        L0 += 1 / (2μ) * sum(pen)
+    end
+    return L0
+end
+
+function grad_LA(prob, x,y,z,μ)
+    ceq = con_eq(prob,x)
+    ∇f = grad_obj(prob, x)
+    ∇ceq = jac_eq(prob, x)
+    grad = ∇f + ∇ceq'*(y + μ*ceq)
+    
+    p = num_cones(prob)
+    if p > 0
+        cones = con_soc(prob, x)
+        pi = length(cones[1])
+        ∇cone = Matrix(I,pi,pi)[:,1:end-1]   # derivative of cone wrt x
+        for i = 1:p
+            qi = prob.qinds[i]
+            csoc = Πsoc(z[i] - μ*cones[i])
+            ∇csoc = jac_soc(z[i] - μ*cones[i])*(-μ*∇cone)
+            ∇pen = ∇csoc'csoc / μ
+            grad[qi] += ∇pen
+        end
+    end
+    return grad
+end
+
+function hess_LA(prob, x,y,z,μ)
+    hess_f = hess_obj(prob,x)
+    ∇ceq = jac_eq(prob, x)
+    p = num_cones(prob)
+    hess = hess_f + μ*∇ceq'∇ceq
+
+    if p > 0
+        cones = con_soc(prob, x)
+        pi = length(cones[1])
+        ∇cone = Matrix(I,pi,pi)[:,1:end-1]   # derivative of cone wrt x
+        for i = 1:p
+            qi = prob.qinds[i]
+            csoc = Πsoc(z[i] - μ*cones[i])
+            ∇csoc = jac_soc(z[i] - μ*cones[i])*(-μ*∇cone)
+            ∇²csoc = μ^2*∇cone'hess_soc(z[i] - μ*cones[i], csoc)*∇cone  # plus the 2nd order cone function 
+            ∇pen = (∇csoc'∇csoc + ∇²csoc) / μ
+            hess[qi,qi] += ∇pen
+        end
+    end
+    return hess
+end
+
+# Test derivatives w/o cones
+D,N = 2,11
+prob = DoubleIntegrator(D,N, gravity=true)
+x = rand(num_vars(prob))
+cones = con_soc(prob, x)
+
+y = rand(num_cons(prob))
+z = rand.(cones)
+μ = rand()*10
+auglag(prob,x,y,z,μ)
+
+LA(x) = auglag(prob, x,y,z,μ)
+ForwardDiff.gradient(LA, x) ≈ grad_LA(prob, x,y,z,μ)
+ForwardDiff.hessian(LA, x) ≈ hess_LA(prob,x,y,z,μ)
+
+# Add in cones
+qinds = [z[1+2D:end] for z in eachcol(LinearIndices(zeros(3D,N)))]
+prob = DoubleIntegrator(D,N, gravity=true, qinds=qinds)
+cones = con_soc(prob, x)
+z = rand.(length.(cones)) 
+num_cones(prob) == N
+
+t = 1.0
+i = 1
+jac_soc.(cones) ≈ ForwardDiff.jacobian.(Πsoc, cones)
+pen(x) = Πsoc(z[i] - μ*[x;t])
+u1 = x[1+2D:3D]
+∇cone = Matrix(I,D+1,D+1)[:,1:end-1]
+ForwardDiff.jacobian(pen, u1) ≈ -μ*jac_soc(z[i] - μ*cones[i])*∇cone
+
+# Move it outside the cone
+u1 *= 10
+cones[i] = [u1;1]
+norm(u1) > 1
+ForwardDiff.jacobian(pen, u1) ≈ -μ*jac_soc(z[i] - μ*cones[i])*∇cone
+ForwardDiff.jacobian(pen, u1) ≈ -μ*jac_soc(z[i] - μ*cones[i])[:,1:end-1]
+
+# Test the augmented Lagrangian expansion
+x = rand(num_vars(prob)) * 10  # make it go outside the cone
+ForwardDiff.gradient(LA, x) ≈ grad_LA(prob, x,y,z,μ)
+ForwardDiff.hessian(LA, x) ≈ hess_LA(prob, x,y,z,μ)
+
+# Test soc projection derivatives
+v = rand(4)
+b = rand(5)
+s = norm(v) * 0.9
+a = [v;s]
+Πsoc(a) ≈ 0.5*[
+    v+s*v/norm(v); 
+    norm(v) + s
+]
+jac_soc(a) ≈ 0.5*[
+    I + s*(I - (v*v')/(v'v))/norm(v)     v/norm(v);
+    v'/norm(v)                            1
+]
+
+bv = b[1:end-1]
+bs = b[end]
+jac_soc(a)'b ≈ 0.5*[
+    bv + s/norm(v)*(I - (v*v')/(v'v))*bv + v/norm(v)*bs;
+    v'bv/norm(v) + bs
+]
+
+ForwardDiff.jacobian(x->jac_soc(x)'b, a) ≈ hess_soc(a,b) 
